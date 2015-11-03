c10a99c3a3d15db68a622ff24ec7feef21a385b7
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-11-03 15:19:37 -0800

    First commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,21 @@
+class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    product_params = params.require(:product).permit([:title, :price, :description])
+    @product       = Product.new product_params
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new # this will render views/products/new.html.erb
+    end
+  end
+
+  def show
+    @product = Product.find params[:id]
+  end
+
+end
